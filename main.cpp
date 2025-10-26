d09353884cfd955bd7448641fbee26e0de9bc5f0
Nafi-GOAT <nafih2989@gmail.com>
2025-10-26 02:19:42 -0700

    3

--- main.cpp
+++ main.cpp
@@ -12,16 +12,19 @@ int main() {
     list<string> lst;
     set<string> st;
 
-    fstream fin("data.txt");
+    ifstream fin("data.txt");
     string word;
     vector<string> words;
     while (fin >> word)
-        words.pop_back(word);
+        words.push_back(word);
     fin.close();
 
     cout << "Data loaded: " << words.size() << " items\n";
 
-
+    auto start = high_resolution_clock::now();
+    for (auto & : words) vec.push_back(w);
+    auto end = high_resolution_clock::now();
+    auto vecRead = duration_cast<microseconds>(end - start).count();
 
 
 
